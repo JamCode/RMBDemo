f3639827c0a74513cac2460f983d5fe2bc0bac10
wanghan <wh85125@163.com>
2016-04-25 13:47:34 +0800

    #

--- backend/router/cb.js
+++ backend/router/cb.js
@@ -107,14 +107,14 @@ router.get('/chart', function(req, res){
     for (var i = 0; i < 25; i++) {
 
         dealData.push({
-            t: parseInt(Date.now() - Math.random()*1000),
-            rate: rateMax+Math.random()*0.1,
-            vol: volumeMax+Math.random()*volumeMin
+            t: parseInt(Date.now() - Math.random()*1000000),
+            rate: parseFloat((rateMax+Math.random()*0.1).toFixed(4)),
+            vol: parseInt(volumeMax+Math.random()*volumeMin)
         });
 
         baseRate.push({
-            t: parseInt(Date.now() - Math.random()*1000),
-            rate: rateMax+Math.random()*0.1,
+            t: parseInt(Date.now() - Math.random()*1000000),
+            rate: parseFloat((rateMax+Math.random()*0.1).toFixed(4)),
         });
 
     }
